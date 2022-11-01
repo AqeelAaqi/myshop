b2d9eda4bdc1f6c1faa197639920a5bbd208312a
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-11-01 15:41:41 +0500

    adding navigation to the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/screens/product_detail_screen.dart';
 import 'package:myshop/screens/product_overview_screen.dart';
 
 void main() {
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
       ),
       // home: const MyHomePage(title: 'MyShop'),
       home:  ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routName: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget{
+  static const String routName = '/product_detail_screen';
+
   @override
   Widget build(BuildContext context) {
+    final String productID = ModalRoute.of(context)?.settings.arguments as String;
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text(''),),
+      appBar: AppBar(title: Text('title'),),
       body: Container(),
     );
   }
