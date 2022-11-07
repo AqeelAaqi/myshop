f152d7adfb3f1e04f3ea62c4f8842aa18b191e4e
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-11-07 16:56:27 +0500

    add detail screen design

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -2,20 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:myshop/providers/products.dart';
 import 'package:provider/provider.dart';
 
-class ProductDetailScreen extends StatelessWidget{
+class ProductDetailScreen extends StatelessWidget {
   static const String routName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
     final productId =
-    ModalRoute.of(context)?.settings.arguments as String; // is the id!
+        ModalRoute.of(context)?.settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
       context,
       listen: false,
     ).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title),),
-      body: Container(),
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('\$ ${loadedProduct.price}',style: TextStyle(color: Colors.grey, fontSize: 20),),
+            SizedBox(height: 10,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(loadedProduct.description, textAlign: TextAlign.center,softWrap: true,),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
