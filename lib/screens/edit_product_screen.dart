b7ec13d71bf49c5b7213510fa3aa41493a996299
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-11-08 18:12:25 +0500

    image input and image oreview

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class EditProductSreen extends StatefulWidget {
-
   static const routeName = '/edit-product';
 
   @override
@@ -11,12 +10,32 @@ class EditProductSreen extends StatefulWidget {
 class _EditProductSreenState extends State<EditProductSreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlControler = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl(){
+    if(!_imageUrlFocusNode.hasFocus){
+      setState(() {
+
+      });
+    }
+  }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlControler.dispose();
     super.dispose();
   }
 
@@ -35,7 +54,7 @@ class _EditProductSreenState extends State<EditProductSreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_){
+                onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
               ),
@@ -44,7 +63,7 @@ class _EditProductSreenState extends State<EditProductSreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
-                onFieldSubmitted: (_){
+                onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
               ),
@@ -53,10 +72,38 @@ class _EditProductSreenState extends State<EditProductSreen> {
                 // textInputAction: TextInputAction.next,
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
-                onFieldSubmitted: (_){
+                focusNode: _descriptionFocusNode,
+                onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
-              )
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(top: 8, right: 10),
+                    decoration:
+                        BoxDecoration(border: Border.all(), color: Colors.grey),
+                    child: _imageUrlControler.text.isEmpty
+                        ? Text('Enter a Url')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlControler.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlControler,
+                    focusNode: _imageUrlFocusNode,
+                  ),)
+                ],
+              ),
             ],
           ),
         ),
