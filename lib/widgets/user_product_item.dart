a5d5a25a49da3bb3168361e89cb4bdc7e88e67f3
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-12-13 19:17:26 +0500

    add custom exception and handle error

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -12,6 +12,7 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -24,14 +25,20 @@ class UserProductItem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.edit),
               onPressed: () {
-                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
               },
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {
-                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              onPressed: () async{
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (Exception) {
+                  scaffold.showSnackBar(SnackBar(content: Text('Deleting failed!')));
+                }
               },
               color: Theme.of(context).errorColor,
             ),
