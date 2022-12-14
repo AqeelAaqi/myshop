a6bd82ad42d0b7ead8f03cceb9af7b78d2b98901
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-12-14 18:38:25 +0500

    set favorite on server

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -19,8 +21,27 @@ class Product with ChangeNotifier {
       }
   );
 
-  void toggelFavotiteStatus(){
+  void _setFavValue (bool newValue){
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggelFavotiteStatus() async{
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    try{
+      Uri url = Uri.parse(
+          'https://flutter-practice-f96f3-default-rtdb.firebaseio.com/products/$id.json');
+      final response = await http.patch(url,body: json.encode({
+        'isFavorite': isFavorite,
+      }));
+      if(response.statusCode >= 400){
+        _setFavValue(oldStatus);
+      }
+    }catch(error){
+      print(error);
+      _setFavValue(oldStatus);
+    }
   }
 }
