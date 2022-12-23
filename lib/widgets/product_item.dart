b97d861e05958d86e19e0d64358b93f1ff98de53
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-12-23 19:07:43 +0500

    fadein image loaded

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,7 +16,12 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(product.imageUrl, fit: BoxFit.fill),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
+            fit: BoxFit.cover,
+          ),
+          // Image.network(product.imageUrl, fit: BoxFit.fill),
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
