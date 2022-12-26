a68c9df7eac4f3c6e2b88e673d5ec1aca529c31a
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-12-26 17:44:53 +0500

    orders animated with orderBox

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -17,40 +17,54 @@ class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$ ${widget.order.amount}'),
-            subtitle: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? min(widget.order.products.length * 20 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('\$ ${widget.order.amount}'),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20 + 10, 100),
-              child: ListView(
-                children: widget.order.products
-                    .map((e) => Row(
+            // if (_expanded)
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                height: _expanded ? min(widget.order.products.length * 20 + 10, 100) : 0,
+                child: ListView(
+                  children: widget.order.products
+                      .map(
+                        (e) => Row(
                           children: <Widget>[
-                            Text(e.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                            Text('${e.quantity} x \$ ${e.price}', style: TextStyle(fontSize: 18, color: Colors.grey),)
+                            Text(
+                              e.title,
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${e.quantity} x \$ ${e.price}',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            )
                           ],
                         ),
-                )
-                    .toList(),
-              ),
-            )
-        ],
+                      )
+                      .toList(),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
